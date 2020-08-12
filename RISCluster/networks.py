3b25628e7204132aadc6e238d50bc32d7ecd42b6
William Jenkins <wjenkins@ucsd.edu>
2020-08-12 09:50:15 -0500

    Remove output_padding from conv2d

--- RISCluster/networks.py
+++ RISCluster/networks.py
@@ -6,9 +6,9 @@ class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 8, kernel_size=5, stride=2, padding=0),
+            nn.Conv2d(1, 8, kernel_size=3, stride=2, padding=1),
             nn.ReLU(True),
-            nn.Conv2d(8, 16, kernel_size=5, stride=2, padding=0),
+            nn.Conv2d(8, 16, kernel_size=3, stride=2, padding=1),
             nn.ReLU(True),
             nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),
             nn.ReLU(True),
@@ -32,24 +32,71 @@ class Decoder(nn.Module):
             nn.ReLU(True)
         )
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=0),
             nn.ReLU(True),
-            nn.ConvTranspose2d(32, 16, kernel_size=5, stride=2, padding=2, output_padding=1),
+            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1),
             nn.ReLU(True),
-            nn.ConvTranspose2d(16, 8, kernel_size=5, stride=2, padding=2, output_padding=1),
+            nn.ConvTranspose2d(16, 8, kernel_size=3, stride=2, padding=1),
             nn.ReLU(True),
-            nn.ConvTranspose2d(8, 1, kernel_size=5, stride=2, padding=2, output_padding=1),
+            nn.ConvTranspose2d(8, 1, kernel_size=3, stride=2, padding=1),
         )
 
     def forward(self, x):
         x = self.latent2dec(x)
         x = x.view(-1, 64, 4, 8)
         x = self.decoder(x)
-        return x
+        return x[:,:,1:,1:]
+        # return x
 
-def init_weights(m):
-    if type(m) in [nn.Linear, nn.Conv2d, nn.ConvTranspose2d]:
-        torch.nn.init.xavier_uniform_(m.weight)
+# Encoder Layers
+# class Encoder(nn.Module):
+#     def __init__(self):
+#         super(Encoder, self).__init__()
+#         self.encoder = nn.Sequential(
+#             nn.Conv2d(1, 8, kernel_size=5, stride=2, padding=0),
+#             nn.ReLU(True),
+#             nn.Conv2d(8, 16, kernel_size=5, stride=2, padding=0),
+#             nn.ReLU(True),
+#             nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),
+#             nn.ReLU(True),
+#             nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+#             nn.ReLU(True),
+#             nn.Flatten(),
+#             nn.Linear(2048, 32),
+#             nn.ReLU(True)
+#         )
+#
+#     def forward(self, x):
+#         x = self.encoder(x)
+#         return x
+#
+# # Decoder Layers
+# class Decoder(nn.Module):
+#     def __init__(self):
+#         super(Decoder, self).__init__()
+#         self.latent2dec = nn.Sequential(
+#             nn.Linear(32, 2048),
+#             nn.ReLU(True)
+#         )
+#         self.decoder = nn.Sequential(
+#             nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),
+#             nn.ReLU(True),
+#             nn.ConvTranspose2d(32, 16, kernel_size=5, stride=2, padding=2, output_padding=1),
+#             nn.ReLU(True),
+#             nn.ConvTranspose2d(16, 8, kernel_size=5, stride=2, padding=2, output_padding=1),
+#             nn.ReLU(True),
+#             nn.ConvTranspose2d(8, 1, kernel_size=5, stride=2, padding=2, output_padding=1),
+#         )
+#
+#     def forward(self, x):
+#         x = self.latent2dec(x)
+#         x = x.view(-1, 64, 4, 8)
+#         x = self.decoder(x)
+#         return x
+#
+# def init_weights(m):
+#     if type(m) in [nn.Linear, nn.Conv2d, nn.ConvTranspose2d]:
+#         torch.nn.init.xavier_uniform_(m.weight)
         # m.bias.data.fill_(0.01)
 
 # Clustering Layer
