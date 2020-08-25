3742681f5375329f2a660d225b18b2550ec44ea5
William Jenkins <wjenkins@ucsd.edu>
2020-08-24 21:24:36 -0700

    Save .ini files to batch directory

--- scripts/make_init_pred_configs.py
+++ scripts/make_init_pred_configs.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.insert(0, '../RISCluster/')
 
-from utils import make_pred_configs
+from utils import make_pred_configs_batch
 
 loadpath = '../../../Outputs/Models/DCM'
 savepath = '../../ConfigFiles'
 overwrite = False
-make_pred_configs(loadpath, savepath, overwrite)
+make_pred_configs_batch(loadpath, savepath, overwrite)
