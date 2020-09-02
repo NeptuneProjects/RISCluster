e9302f99a00cae6d7b836174fbe536662254921a
William Jenkins <wjenkins@ucsd.edu>
2020-09-02 15:48:02 -0700

    Workflow absorbed by runBatchEval

--- scripts/make_init_pred_configs.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0, '../RISCluster/')
-
-from utils import make_pred_configs_batch
-
-loadpath = '../../../Outputs/Models/DCM/Exp20200823T170830'
-savepath = '../../ConfigFiles'
-overwrite = False
-init_path = make_pred_configs_batch(loadpath, savepath, overwrite)
