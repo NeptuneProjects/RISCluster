b0b72b19d944f7c5b17a6c8163ce2dd6b056ff42
William Jenkins <wjenkins@ucsd.edu>
2020-09-03 01:11:26 -0700

    T/S

--- RISCluster/models.py
+++ RISCluster/models.py
@@ -625,9 +625,7 @@ def gmm(model, dataloader, device):
         x = batch.to(device)
         _, _, z = model(x)
         z_array[b * x.size(0):(b+1) * x.size(0), :] = z.detach().cpu().numpy()
-    print(np.any(np.isnan(z_array)))
-    print(np.all(np.isfinite(z_array)))
-    np.seterr(under='warn')
+    np.seterr(under='ignore')
     labels = GMM.fit_predict(z_array)
     centroids = GMM.means_
     return labels, centroids
@@ -694,6 +692,7 @@ def predict_labels(model, dataloader, device):
         q_array[b * x.size(0):(b+1) * x.size(0), :] = q.detach().cpu().numpy()
 
     labels = np.argmax(q_array.data, axis=1)
+    print(np.sum(q_array(axis=1)))
     return np.round(q_array, 5), labels
 
 def target_distribution(q):
