2ab195563a5a30b64cc3714e7925f7bc1b78bb71
William Jenkins <wjenkins@ucsd.edu>
2020-09-03 01:22:03 -0700

    T/S

--- RISCluster/models.py
+++ RISCluster/models.py
@@ -11,6 +11,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+np.set_printoptions(threshold=sys.maxsize)
 if sys.platform == 'darwin':
     from sklearn.cluster import KMeans
     from sklearn.manifold import TSNE
