e7d78481ecedf64bdd7f442ab1cfcb8785e6ddda
William Jenkins <wjenkins@ucsd.edu>
2020-09-09 20:26:58 -0700

    T/S

--- RISCluster/utils.py
+++ RISCluster/utils.py
@@ -224,8 +224,9 @@ def make_pred_configs_batch(loadpath, savepath, overwrite=False):
         os.makedirs(savepath)
 
     config_ = configparser.ConfigParser()
-    config_.read(f"{loadpath}/*.ini")
-    print(f"{loadpath}/*.ini")
+    tmp = [f for f in os.listdir(loadpath) if f.endswith('.ini')][0]
+    config_.read(tmp)
+    print(tmp)
     transform = config_['PARAMETERS']['transform']
 
     count_wr = 0
