eacf9a638527b5711e1c4def5b3aa8776b545425
William Jenkins <wjenkins@ucsd.edu>
2020-11-18 13:39:06 -0600

    T/S latex

--- RISCluster/plotting.py
+++ RISCluster/plotting.py
@@ -942,7 +942,7 @@ def view_latent_space(
             plt.yticks(ticks=np.linspace(0,d-1,d), labels=np.linspace(1,d,d, dtype='int'), size=5)
             plt.ylabel('Before DEC', size=12, y=0.6)
             plt.rcParams.update(params)
-            plt.title(f"$\\pmb{{\mu}}_j$", size=14)
+            plt.title(fr"$\\pmb{{\\mu}}_j$", size=14)
         else:
             plt.yticks(ticks=np.linspace(0,d-1,d), labels=[], size=5)
 
