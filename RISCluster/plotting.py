ea84a04b3c66d580185b4cf3ad10df3f60ea3670
William Jenkins <wjenkins@ucsd.edu>
2020-11-18 15:57:05 -0600

    t/s latex

--- RISCluster/plotting.py
+++ RISCluster/plotting.py
@@ -940,7 +940,7 @@ def view_latent_space(
             plt.ylabel('Before DEC', size=12, y=0.6)
             plt.rc('text', usetex=True)
             plt.rc('text.latex', preamble=[r'\usepackage{amsmath}', r'\usepackage{bm}'])
-            plt.title(r"\$\$\bm{\mu}_j\$\$")
+            plt.title(r"$\bm{\mu}_j$")
             # plt.title("$\\boldsymbol{\\mu}_j$", size=14)
             # plt.title(r"$\boldsymbol{\mu}_j$", size=14)
             # plt.title(fr"$\mu_j$", size=14) <---- works
