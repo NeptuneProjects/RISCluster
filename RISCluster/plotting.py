ecc775de1e82b7312b9c69592e3ba6299cdd4b21
William Jenkins <wjenkins@ucsd.edu>
2020-11-18 15:24:59 -0600

    t/s latex

--- RISCluster/plotting.py
+++ RISCluster/plotting.py
@@ -908,7 +908,7 @@ def view_latent_space(
     fig = plt.figure(figsize=(8, 2.5*nrows), dpi=150)
     params = {
         'text.usetex': True,
-        'text.latex.preamble': '\\usepackage{bm}'
+        'text.latex.preamble': ['\\usepackage{amsmath}', '\\usepackage{bm}']
     }
     gs = gridspec.GridSpec(nrows=nrows, ncols=2, height_ratios=heights, hspace=0.3, wspace=0.05)
     cmap = 'cmo.deep_r'
@@ -939,7 +939,7 @@ def view_latent_space(
             plt.yticks(ticks=np.linspace(0,d-1,d), labels=np.linspace(1,d,d, dtype='int'), size=5)
             plt.ylabel('Before DEC', size=12, y=0.6)
             plt.rcParams.update(params)
-            plt.title(r"$ \bm{\mu}_j$")
+            plt.title("$\\bm{\\mu}_j$")
             # plt.title("$\\boldsymbol{\\mu}_j$", size=14)
             # plt.title(r"$\boldsymbol{\mu}_j$", size=14)
             # plt.title(fr"$\mu_j$", size=14) <---- works
