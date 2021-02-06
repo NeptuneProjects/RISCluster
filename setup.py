ca29b6ed96a9f67c0796e0ba5054ecfb7e156e43
NeptuneProjects <william.f.jenkins@gmail.com>
2021-02-05 16:09:31 -0800

    ts

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def setup_package():
                 'query_H5size=RISCluster.utils:query_H5size',
                 'ExtractH5Dataset=RISCluster.utils:ExtractH5Dataset',
                 'GenerateSampleIndex=RISCluster.utils:GenerateSampleIndex',
-                'testmain=RISCluster.testmain'
+                # 'testmain=RISCluster.testmain'
             ]
         },
         install_requires=[
