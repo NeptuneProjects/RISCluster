9ece02c16a2e8e6d4f5b637a3779707fb55ff699
NeptuneProjects <william.f.jenkins@gmail.com>
2021-04-12 16:53:33 -0700

    ts cli

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def setup_package():
         author_email='wjenkins@ucsd.edu',
         packages=['RISCluster'],
         # scripts=["scripts/testscript.py", "scripts/testscript2.py"],
-        scripts=['RISCluster/runDEC'],
+        scripts=['RISCluster/runDEC.py'],
         entry_points = {
             'console_scripts': [
                 # 'runDEC=RISCluster.runDEC:main',
